6e0ef111bb7057aa589b5007a183dc9cf292c787
Farid <famimahe2003@gmail.com>
2024-07-02 11:54:07 -0600

    test

--- src/app/components/tabdiv/tabdiv.component.ts
+++ src/app/components/tabdiv/tabdiv.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, ContentChild, Input, OnInit, TemplateRef } from '@angular/core';
+import { RouteConfigLoadEnd, RouteConfigLoadStart, Router, RouterLink, RouterOutlet } from '@angular/router';
+import { LoadingscreenService } from '../services/loadingscreen.service';
+import { Observable, tap } from 'rxjs';
 
 @Component({
   selector: 'app-tabdiv',
   standalone: true,
-  imports: [],
+  imports: [RouterLink,RouterOutlet,],
   templateUrl: './tabdiv.component.html',
   styleUrl: './tabdiv.component.css'
 })
-export class TabdivComponent {
+export class TabdivComponent implements OnInit{
+  loading$: Observable<boolean>;
 
+  @Input()
+  detectRouteTransitions = false;
+
+  @ContentChild("loading")
+  customLoadingIndicator: TemplateRef<any> | null = null;
+
+  constructor(
+  private loadingService: LoadingscreenService,
+  private router: Router) {
+    this.loading$ = this.loadingService.loading$;
+  }
+
+  ngOnInit() {
+    if (this.detectRouteTransitions) {
+      this.router.events
+        .pipe(
+          tap((event) => {
+            if (event instanceof RouteConfigLoadStart) {
+              this.loadingService.loadingOn();
+            } else if (event instanceof RouteConfigLoadEnd) {
+              this.loadingService.loadingOff();
+            }
+          })
+        )
+        .subscribe();
+    }
+  }
 }
