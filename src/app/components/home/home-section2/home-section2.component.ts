730f2be6aa2f9bfe8e9370b0add475dc885dee53
pajaro <root@local>
2024-07-08 12:28:51 -0600

    valdivia

---
+++ src/app/components/home/home-section2/home-section2.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-section2',
+  standalone: true,
+  imports: [],
+  templateUrl: './home-section2.component.html',
+  styleUrl: './home-section2.component.css'
+})
+export class HomeSection2Component {
+
+}
