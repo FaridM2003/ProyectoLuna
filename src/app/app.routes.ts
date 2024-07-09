d2f848b2099ad72bb32b1012f58f28878ef13628
Farid <famimahe2003@gmail.com>
2024-07-09 14:05:59 -0600

    solucion de problema por el tabdiv

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,3 @@
-import { TabdivComponent } from './components/tabdiv/tabdiv.component';
 import { AboutitComponent } from './components/aboutit/aboutit.component';
 import { HomeComponent } from './components/home/home.component';
 import { Routes, RouterLink } from '@angular/router';
